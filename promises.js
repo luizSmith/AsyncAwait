da61e68e0d48f6c0d1097578c71945da6024942a
Luiz Victor <luiz.victor100@hotmail.com>
2020-04-15 20:30:27 -0300

    Promises alinhadas

--- promises.js
+++ promises.js
@@ -1,3 +1,19 @@
+function pegarId(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(5);
+        },2500);
+    });
+}
+
+function buscarEmailNoBanco(id) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("luiz.victor100@hotmail.com")
+        },1000);
+    })
+}
+
 function enviarEmail(corpo, para) {
     return new Promise((resolve, reject) => {
         setTimeout(() => { //Arrow function
@@ -14,15 +30,26 @@ function enviarEmail(corpo, para) {
     });
 }
 
-enviarEmail("Olá mundo","luiz.victor100@hotmail").then((dados) => {
+// enviarEmail("Olá mundo","luiz.victor100@hotmail").then( ({ ass, to}) => { //.then((dados) => { 
 
-    console.log(`
-        Assunto: ${dados.assunto}
-        ------------------------
-        To: ${dados.to}
-    `);
+//     console.log(`
+//         Assunto: ${ass}
+//         ------------------------
+//         To: ${to}
+//     `);
 
-}).catch(() => {
-    console.log("Não foi dessa vez :(");
-});
+// }).catch(() => {
+//     console.log("Não foi dessa vez :(");
+// });
 
+pegarId().then((id) => {
+    buscarEmailNoBanco(id).then((email) => {
+
+        enviarEmail("Olá, como você está ?",email).then(() => {
+            console.log("Email enviado com sucesso para " + email);
+        }).catch(err => {
+            console.log(err);
+        })
+
+    })
+})
